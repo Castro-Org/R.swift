8286dcb853519611414c4490da02927000b144cf
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2023-09-28 00:40:03 +0300

    Changed the output folder for `R.generated.swift` file for Xcode Cloud

--- Plugins/RswiftGeneratePublicResources/RswiftGeneratePublicResources.swift
+++ Plugins/RswiftGeneratePublicResources/RswiftGeneratePublicResources.swift
@@ -12,8 +12,12 @@ import PackagePlugin
 struct RswiftGeneratePublicResources: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         guard let target = target as? SourceModuleTarget else { return [] }
-
-        let outputDirectoryPath = context.pluginWorkDirectory
+        
+        /// On Xcode cloud we don't have write access to the derived data folder, which is the default output location for the `R.generated.swift` file.
+        /// With this change we are saving the file to a `tmp` folder, which we access to by default both on local machines and on Xcode Cloud
+        ///
+        ///  - Note: It's better-engineering to tie this behaviour to a build setting like `R_SWIFT_USE_TMP_FOLDER` to be backwards compatible with `R.swift` itself but it doesn't hurt us this way either
+        let outputDirectoryPath = Path(NSTemporaryDirectory())
             .appending(subpath: target.name)
 
         try FileManager.default.createDirectory(atPath: outputDirectoryPath.string, withIntermediateDirectories: true)
@@ -85,3 +89,30 @@ extension RswiftGeneratePublicResources: XcodeBuildToolPlugin {
 }
 
 #endif
+
+public extension ProcessInfo {
+    var isLikelyXcodeCloudEnvironment: Bool {
+        // https://developer.apple.com/documentation/xcode/environment-variable-reference
+        let requiredKeys: Set = [
+            "CI",
+            "CI_BUILD_ID",
+            "CI_BUILD_NUMBER",
+            "CI_BUNDLE_ID",
+            "CI_COMMIT",
+            "CI_DERIVED_DATA_PATH",
+            "CI_PRODUCT",
+            "CI_PRODUCT_ID",
+            "CI_PRODUCT_PLATFORM",
+            "CI_PROJECT_FILE_PATH",
+            "CI_START_CONDITION",
+            "CI_TEAM_ID",
+            "CI_WORKFLOW",
+            "CI_WORKSPACE",
+            "CI_XCODE_PROJECT",
+            "CI_XCODE_SCHEME",
+            "CI_XCODEBUILD_ACTION"
+        ]
+        
+        return requiredKeys.isSubset(of: environment.keys)
+    }
+}
